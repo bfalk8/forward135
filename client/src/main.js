14704a74b6462cb8a885e1aeb29d0fd8760de9e8
Kyly <kylyvass@gmail.com>
2016-05-08 11:57:49 -0700

    Fixed for ec 6

--- client/src/main.js
+++ client/src/main.js
@@ -1,24 +1,16 @@
 import Greeting from './Greeting.js';
 import io from 'socket.io-client';
 
-localStorage.debug = true;
 let socket = io(`http://localhost:3000`);
+localStorage.debug = true;
 
-socket.on('connect', function() {
-    console.log('connected');
-});
+socket.on('connect', () => console.log('connected'));
 
-socket.on('message', function(msg){
-    console.log(msg);
-});
+socket.on('message', msg => console.log(msg) );
 
-socket.on('disconnect', function() {
-    console.log('disconnected');
-});
+socket.on('disconnect', () => console.log('disconnected') );
 
-socket.on('error', function (e) {
-    console.log('System', e ? e : 'A unknown error occurred');
-});
+socket.on('error', e => console.log('System', e ? e : 'A unknown error occurred'));
 
 var h1 = document.querySelector('h1');
 h1.textContent = new Greeting();
