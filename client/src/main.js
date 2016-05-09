7ef3b47ee6ddb09bd8e7bcfcf42dc8d3021ae652
Kyly <kylyvass@gmail.com>
2016-05-08 19:08:35 -0700

    Encapsulated web socket in its own class. Made it a singleton.

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,4 @@
 import Greeting from './Greeting.js';
-import io from 'socket.io-client';
-
-let socket = io(`http://localhost:3000`);
-localStorage.debug = true;
-
-socket.on('connect', () => console.log('connected'));
-
-socket.on('message', msg => console.log(msg) );
-
-socket.on('disconnect', () => console.log('disconnected') );
-
-socket.on('error', e => console.log('System', e ? e : 'A unknown error occurred'));
-
+import WebSocket from './WebSocket.js';
 var h1 = document.querySelector('h1');
 h1.textContent = new Greeting();
