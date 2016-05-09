b23d38f4763f29ae0b233451ffcf7be3b76e558a
Kyly <kylyvass@gmail.com>
2016-05-08 19:08:35 -0700

    Encapsulated web socket in its own class. Made it a singleton.

---
+++ client/src/WebSocket.js
@@ -0,0 +1,26 @@
+import io from 'socket.io-client';
+class WebSocket {
+
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        let host = window.location.origin;
+        console.log("WEBSOCKET connecting to", host);
+
+        this.socket = io.connect(host);
+
+        this.socket.on('connect', () => {
+            let sessionId = this.socket.io.engine.id;
+            console.log("WEBSOCKET connected with session id", sessionId);
+        });
+
+        this.socket.on('disconnect', () => console.log('disconnected') );
+
+        this.socket.on('error', e => console.log('System', e ? e : 'A unknown error occurred'));
+    }
+}
+
+let websocket = new WebSocket();
+export default websocket;
