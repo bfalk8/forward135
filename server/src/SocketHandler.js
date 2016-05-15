0e80184cb17e2a6fda81b7bb61021c1e0073856c
bfalk8 <bfalk@ucsd.edu>
2016-05-14 19:19:06 -0700

    The scariest part of my day....

--- server/src/SocketHandler.js
+++ server/src/SocketHandler.js
@@ -1,41 +1,50 @@
 'use strict';
-//import * as socket from 'socket.io';
+
 var socket = require('socket.io');
-var ivm    = require('./IVM');
+var IVM = require('./IVM');
+let self = null;
 
 class SocketHandler {
-    constructor(http) {
-        this.io = socket(http);
-        this.io.on('connection', (socket) => {
-            this.handleSocket(socket)
-        });
+    constructor(http){
+        if(!self) {
+            this.io = socket(http);
+            this.ivm = new IVM();
+            this.io.on('connection', this.handleSocket);
+            self = this;
+        }
+        return self;
     }
 
     handleSocket(socket) {
-        console.log("connected");
-        socket.on('echo', (data)=> {
-            this.echo(socket, data)
-        });
-        socket.on('init query', (data) => {
-            this.checkRoom(socket, data)
-        });
+        console.log('connected');
+        socket.on('echo', (data)=>{self.echo(socket, data)});
+        socket.on('init query', (data) => {self.checkRoom(socket, data)});
     }
 
-    checkRoom(socket, data) {
-        console.log("CHECK ROOM: " + data);
-        var query = data.query;
-        if (this.io.rooms.indexOf(query) >= 0)
-        {
-            socket.join(query);
-            socket.emit('init query', {data: 'initial query'});
+    checkRoom(socket, data){
+        console.log('CHECK ROOM: ' + data);
+        var query = data;
+        if(!this.io.sockets.adapter.rooms[query]){
+            console.log('Room: ' + query + ' created!');
+            this.ivm.addQuery(query);
+        } else {
+            console.log('Room: ' + query + ' exists!');
         }
-
+        new IVM().addQuery(query);
+        socket.join(query);
+        socket.emit('init query', {data: 'initial query result'});
+        socket.emit('diff query', new IVM().getQuery(query));
     }
 
     echo(socket, data) {
         socket.emit('echo', data);
     }
 
+    sendQueryDiff(diff) {
+        var query = diff.query;
+        this.io.to(query).emit(diff);
+    }
+
 }
 
-module.exports = SocketHandler;
+module.exports =  SocketHandler;
