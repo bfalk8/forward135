a531d049a17fbb6c43fe494ed0d41e1fa66042bd
bfalk8 <bfalk@ucsd.edu>
2016-05-28 12:34:24 -0700

    connected the IVM with the DatabaseListener

--- server/src/DatabaseListener.js
+++ server/src/DatabaseListener.js
@@ -1,17 +1,27 @@
 'use strict';
 
-var pg = require ('pg');
+var pg = require('pg');
+var ivm = require('./IVM');
 var pgConString = "postgres://postgres:test@localhost:5432/forward135";
 
 class DatabaseListener{
-    constructor(){
-        pg.connect(pgConString, function(err, client) {
+    constructor(dbString){
+        pg.connect(dbString, function(err, client) {
             if(err) {
                 console.log(err);
             }else{
                 client.on('notification', function(msg) {
                     console.log(msg);
+                    if(msg.channel === 'watchers'){
+                        var payload = msg.payload.split(',');
+                        var updateInfo = {};
+                        updateInfo.table = payload[0];
+                        updateInfo.column = payload[1];
+                        updateInfo.id = payload[2];
+                        ivm.tableUpdate(updateInfo);
+                    }
                 });
+                // needs to be here to start listening to 'watchers' channel
                 var query = client.query("LISTEN watchers");
             }
         });
--- server/src/Main.js
+++ server/src/Main.js
@@ -4,14 +4,16 @@
 var SocketHandler = require('./SocketHandler');
 var RandomUpdater = require('./RandomUpdater');
 var Database      = require('./MockDatabase');
+var DatabaseListener = require('./DatabaseListener');
 
 class Main {
 
-    constructor(http) {
+    constructor(http, dbString) {
         this.socket = new SocketHandler(http);
         this.database      = new Database(30);
         this.ivm           = require('./IVM');
         this.updater       = new RandomUpdater(this.database, -1,  5000, this.ivm);
+        this.dblistener    = new DatabaseListener(dbString);
     }
 
     run() {
