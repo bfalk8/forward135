6c6dcaf60d0ac847948023f096f60a9ba6fdf5e2
bfalk8 <bfalk8@gmail.com>
2016-05-28 13:12:44 -0700

    Made a basic Database Query class, needs a lot of work

---
+++ server/src/DatabaseQuery.js
@@ -0,0 +1,22 @@
+'use strict';
+var pg = require('pg');
+
+class DatabaseQuery{
+    constructor(constring, query){
+        pg.connect(constring, function(err, client, done) {
+           if(err){
+               console.log(err);
+           } else {
+               var q = client.query(query);
+               q.on('row', function(row){
+                   console.log('query result: ', row);
+               });
+               q.on('error', function () {
+                   console.log('query failed to execute');
+               });
+           }
+        });
+    }
+}
+
+module.exports = DatabaseQuery;
