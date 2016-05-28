ac7a65ff2c5d9261cd5b9878f8926961f7afe738
Kyly <kylyvass@gmail.com>
2016-05-28 13:13:26 -0700

    Merge branch 'master' of https://github.com/bfalk8/forward135
    Conflicts:
    	index.js
    	server/src/Main.js

--- server/src/IVM.js
+++ server/src/IVM.js
@@ -2,13 +2,12 @@
 
 // let instance      = null;
 var SocketHandler = require('./SocketHandler');
-function IVM() {
-    var ivm      = this;
-    var socketHandler = new SocketHandler();
+const IVM = {
+    socketHandler: new SocketHandler(),
 
-    ivm.queries = {};
+    queries: {},
 
-    ivm.addQuery = (query) =>
+    addQuery: (query) =>
     {
         this.queries[query] = {
             query: query,
@@ -16,39 +15,12 @@ function IVM() {
             updateDiff: 'hey you got an update',
             deleteDiff: 'hey you got a delete'
         };
-    };
+    },
 
-    ivm.sendInsertDiff = (query) =>
-    {
-        // console.log('here we are in send insert diff');
-        socketHandler.sendQueryDiff(ivm.queries[query]);
-    };
-
-    ivm.changeInsertDiff = (query, diff) =>
-    {
-        ivm.queries[query].insertDiff = diff;
-        ivm.sendInsertDiff(query);
-    };
-
-    ivm.changeUpdateDiff = (query, diff) =>
-    {
-        ivm.queries[query].updateDiff = diff;
-    };
-
-    ivm.changeDeleteDiff = (query, diff) =>
-    {
-        ivm.queries[query].deleteDiff = diff;
-    };
-
-    ivm.getQuery = (query) =>
-    {
-        return ivm.queries[query];
-    };
-
-    ivm.tableUpdate = (change) =>
+    tableUpdate: (change) =>
     {
         console.log(change);
-    };
-}
+    }
+};
 
-module.exports = new IVM();
+module.exports = IVM;
