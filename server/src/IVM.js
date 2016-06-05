d9e31c17b7d3b141533a45f4926738810131f128
bfalk8 <bfalk8@gmail.com>
2016-06-04 23:53:49 -0700

    Beginnings of the materialized view implementation.

--- server/src/IVM.js
+++ server/src/IVM.js
@@ -2,21 +2,26 @@
 
 var SocketHandler = require('./SocketHandler');
 var DatabaseQuery = require('./DatabaseQuery');
-
+var DAO = require('./DAO');
 let Tables        = require('./Tables');
 
 
 const IVM = {
     queries: {},
     tables: {},
+    viewNum: 0,
+    numUpdates: 0,
+    tolerance: 15,
 
     init: () => {
         this.tables = new Tables();
         this.socketHandler = new SocketHandler();
+        this.viewNum = 0;
     },
 
     addQuery: (tName, query) => {
-        this.tables.table(tName).query = query;
+        this.tables.table(tName).query = {query: query, view: `mv${++this.viewNum}`};
+        DAO.makeMaterializedView(query,`mv${this.viewNum}` );
         console.log('added query');
     },
 
@@ -26,14 +31,17 @@ const IVM = {
      * @param change
      */
     tableUpdate: change => {
-        var queries = this.tables.table(change.table).queries;
-        if (queries.length < 1) {
-            return;
+        if(++this.numUpdates >= 6){
+            var queries = this.tables.table(change.table).queries;
+            if (queries.length < 1) {
+                return;
+            }
+            queries.forEach( element => {
+                DAO.refreshMaterializedView(element.view);
+                // DAO.makeParameterizedQuery(element.query);
+                this.socketHandler.sendQueryDiff(element, change);
+            });
         }
-        console.log(queries);
-        queries.forEach((element, index, array) => {
-            this.socketHandler.sendQueryDiff(element, change);
-        });
     }
 };
 
