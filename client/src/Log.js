44b8d74e50807f5b449cabcd3a71de151c9daf53
Alex Rosengarten <alexrosengarten@gmail.com>
2016-06-07 02:03:18 -0700

    Created analytics table that works with real aggregated data; displays it well. Includes product totals row. Need to add user totals col.

--- client/src/Log.js
+++ client/src/Log.js
@@ -1,6 +1,6 @@
 class Log {
     constructor(listId){
-        this.listId = listId;
+        this.tableId = listId;
     }
     
     appendLog(payload){
@@ -9,7 +9,7 @@ class Log {
         var content = document.createTextNode(JSON.stringify(payload));
         pre.appendChild(content);
         node.appendChild(pre);                            
-        document.getElementById(this.listId).appendChild(node); 
+        document.getElementById(this.tableId).appendChild(node); 
     }
 }
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,46 @@
 import Greeting from './Greeting.js';
 import WebSocket from './WebSocket.js';
 import Log from './Log';
+import AnalyticsTable from './AnalyticsTable';
 
 var h1 = document.querySelector('h1');
 h1.textContent = new Greeting();
 
 let socket = WebSocket.socket;
 
-socket.emit('init query', {table: 'orders', query: 'select * from orders'});
+var top_k_user = 'SELECT USR.NAME AS USER_NAME, PROD.NAME AS PRODUCT_NAME, COALESCE(SUM(O.PRICE), 0) AS CELL_SUM,' +
+'USR.USER_SUM AS ROW_SUM, PROD.PRODUCT_SUM AS COLUMN_SUM' +
+'FROM (' +
+    'SELECT U.ID, U.NAME, SUM(O.PRICE) AS USER_SUM' +
+'FROM USERS U, ORDERS O' +
+'WHERE O.USER_ID = U.ID' +
+'GROUP BY U.ID, U.NAME' +
+'ORDER BY USER_SUM DESC LIMIT 20' +
+') USR' +
+'CROSS JOIN (' +
+    'SELECT P.ID, P.NAME, SUM(O.PRICE) AS PRODUCT_SUM' +
+'FROM PRODUCTS P, ORDERS O' +
+'WHERE O.PRODUCT_ID = P.ID' +
+'GROUP BY P.ID, P.NAME' +
+'ORDER BY PRODUCT_SUM DESC LIMIT 10' +
+') PROD' +
+'LEFT JOIN ORDERS O' +
+'ON O.USER_ID = USR.ID AND O.PRODUCT_ID = PROD.ID' +
+'GROUP BY USR.ID, PROD.ID, USR.NAME, PROD.NAME, USR.USER_SUM, PROD.PRODUCT_SUM' +
+'ORDER BY USR.USER_SUM DESC';
+
+var lookup_table = 'select * from top_k_lookup;';
+
+var simple_query = 'select * from orders where product_id = 3';
+
+socket.emit('init query', {table: 'orders', query: lookup_table});
+
+var analyticsTable = new AnalyticsTable('analytics');
+
+socket.on('init query', data => {
+    console.log('Populating table');
+    analyticsTable.populate(data.data.payload, 'User \\ Product');
+});
 
 var insertLog = new Log('insertLog');
 socket.on('diff query', (data) => {
