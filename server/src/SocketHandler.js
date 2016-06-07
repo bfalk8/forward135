0beaf14598fc1784f4d0cbe5f4b7803b7101cc3f
Kyly <kylyvass@gmail.com>
2016-06-06 23:05:51 -0700

    Started room hash

--- server/src/SocketHandler.js
+++ server/src/SocketHandler.js
@@ -1,14 +1,17 @@
 'use strict';
 
-var socket = require('socket.io');
+let socket = require('socket.io');
+let sha1   = require('sha1');
+let queryMap = {};
 // var IVM = require('./IVM');
 // let handler = null;
 var handler;
 class SocketHandler {
 
-    constructor(http){
-        if(!handler) {
-            this.io = socket(http);
+    constructor(http) {
+        if (!handler)
+        {
+            this.io  = socket(http);
             this.ivm = require('./IVM');
             this.io.on('connection', this.handleSocket);
             handler = this;
@@ -19,20 +22,29 @@ class SocketHandler {
 
     handleSocket(socket) {
         console.log('connected');
-        socket.on('echo', (data)=>{handler.echo(socket, data)});
-        socket.on('init query', (data) => {handler.checkRoom(socket, data)});
+        socket.on('echo', (data)=> {
+            handler.echo(socket, data)
+        });
+        socket.on('init query', (data) => {
+            handler.checkRoom(socket, data)
+        });
     }
 
-    checkRoom(socket, data){
+    checkRoom(socket, data) {
         console.log('CHECK ROOM: ' + data);
-        var table = data.table;
-        var query = data.query;
-        if(!this.io.sockets.adapter.rooms[query]){
+        var table    = data.table;
+        var query    = data.query;
+        let roomHash = sha1(query);
+        queryMap[query] = roomHash;
+        if (!this.io.sockets.adapter.rooms[roomHash])
+        {
             console.log('Room: ' + query + ' created!');
-            handler.ivm.addQuery(table, query, (results)=>{
+            handler.ivm.addQuery(table, query, (results)=> {
                 socket.emit('init query', {data: results});
             });
-        } else {
+        }
+        else
+        {
             console.log('Room: ' + query + ' exists!');
         }
 
@@ -46,6 +58,7 @@ class SocketHandler {
 
     sendQueryDiff(query, diff) {
         // console.log('here we are in socket', diff);
+        // FIXME map
         handler.io.to(query).emit('diff query', diff);
     }
 
