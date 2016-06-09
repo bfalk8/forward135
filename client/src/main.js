9d683f4608690ad79317ab7ede5a2e6ed89a5569
Alex Rosengarten <alexrosengarten@gmail.com>
2016-06-08 21:21:02 -0700

    changed query string... now gets proper aggregate informaion

--- client/src/main.js
+++ client/src/main.js
@@ -8,39 +8,39 @@ h1.textContent = new Greeting();
 
 let socket = WebSocket.socket;
 
-var top_k_user = 'SELECT USR.NAME AS USER_NAME, PROD.NAME AS PRODUCT_NAME, COALESCE(SUM(O.PRICE), 0) AS CELL_SUM,' +
-'USR.USER_SUM AS ROW_SUM, PROD.PRODUCT_SUM AS COLUMN_SUM' +
-'FROM (' +
-    'SELECT U.ID, U.NAME, SUM(O.PRICE) AS USER_SUM' +
-'FROM USERS U, ORDERS O' +
-'WHERE O.USER_ID = U.ID' +
-'GROUP BY U.ID, U.NAME' +
-'ORDER BY USER_SUM DESC LIMIT 20' +
-') USR' +
-'CROSS JOIN (' +
-    'SELECT P.ID, P.NAME, SUM(O.PRICE) AS PRODUCT_SUM' +
-'FROM PRODUCTS P, ORDERS O' +
-'WHERE O.PRODUCT_ID = P.ID' +
-'GROUP BY P.ID, P.NAME' +
-'ORDER BY PRODUCT_SUM DESC LIMIT 10' +
-') PROD' +
-'LEFT JOIN ORDERS O' +
-'ON O.USER_ID = USR.ID AND O.PRODUCT_ID = PROD.ID' +
-'GROUP BY USR.ID, PROD.ID, USR.NAME, PROD.NAME, USR.USER_SUM, PROD.PRODUCT_SUM' +
-'ORDER BY USR.USER_SUM DESC';
-
+var top_k_user =
+    'SELECT USR.NAME AS USER_NAME, PROD.NAME AS PRODUCT_NAME, COALESCE(SUM(O.PRICE), 0) AS CELL_SUM, ' +
+'USR.USER_SUM AS ROW_SUM, PROD.PRODUCT_SUM AS COLUMN_SUM ' +
+'FROM ( ' +
+    'SELECT U.ID, U.NAME, SUM(O.PRICE) AS USER_SUM ' +
+'FROM USERS U, ORDERS O ' +
+'WHERE O.USER_ID = U.ID ' +
+'GROUP BY U.ID, U.NAME ' +
+'ORDER BY USER_SUM DESC LIMIT 20 ' +
+') USR ' +
+'CROSS JOIN ( ' +
+    'SELECT P.ID, P.NAME, SUM(O.PRICE) AS PRODUCT_SUM ' +
+'FROM PRODUCTS P, ORDERS O ' +
+'WHERE O.PRODUCT_ID = P.ID ' +
+'GROUP BY P.ID, P.NAME ' +
+'ORDER BY PRODUCT_SUM DESC LIMIT 10 ' +
+') PROD ' +
+'LEFT JOIN ORDERS O ' +
+'ON O.USER_ID = USR.ID AND O.PRODUCT_ID = PROD.ID ' +
+'GROUP BY USR.ID, PROD.ID, USR.NAME, PROD.NAME, USR.USER_SUM, PROD.PRODUCT_SUM ' +
+'ORDER BY PROD.PRODUCT_SUM DESC;'
 var lookup_table = 'select * from top_k_lookup;';
 
-var simple_query = 'select * from orders where product_id = 3';
+var simple_query = 'select * from orders ';
 
-socket.emit('init query', {table: 'orders', query: lookup_table});
+socket.emit('init query', {table: 'orders', query: top_k_user});
 
 var analyticsTable = new AnalyticsTable('analytics');
 
-socket.on('init query', data => {
-    console.log('Populating table');
-    analyticsTable.populate(data.data.payload, 'User \\ Product');
-});
+// socket.on('init query', data => {
+//     console.log('Populating table');
+//     analyticsTable.populate(data.data.payload, 'User \\ Product');
+// });
 
 var insertLog = new Log('insertLog');
 socket.on('diff query', (data) => {
