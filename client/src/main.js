0a83cc8de0ee95663e87bf4830b8c81895931c43
Alex Rosengarten <alexrosengarten@gmail.com>
2016-06-09 23:40:08 -0700

    added 10 most recent purchases table... not updating for some reason. Also, header seems to be at the bottom.

--- client/src/main.js
+++ client/src/main.js
@@ -2,24 +2,25 @@ import Greeting from './Greeting.js';
 import WebSocket from './WebSocket.js';
 import Log from './Log';
 import AnalyticsTable from './AnalyticsTable';
+import RecentOrdersTable from './RecentOrdersTable';
 
 var h1 = document.querySelector('h1');
 h1.textContent = new Greeting();
 
 let socket = WebSocket.socket;
 
-var top_k_user =
+var topKUser =
     'SELECT USR.NAME AS USER_NAME, PROD.NAME AS PRODUCT_NAME, COALESCE(SUM(O.PRICE), 0) AS CELL_SUM, ' +
     'USR.USER_SUM AS ROW_SUM, PROD.PRODUCT_SUM AS COLUMN_SUM ' +
     'FROM ( ' +
-        'SELECT U.ID, U.NAME, SUM(O.PRICE) AS USER_SUM ' +
+    'SELECT U.ID, U.NAME, SUM(O.PRICE) AS USER_SUM ' +
     'FROM USERS U, ORDERS O ' +
     'WHERE O.USER_ID = U.ID ' +
     'GROUP BY U.ID, U.NAME ' +
     'ORDER BY USER_SUM DESC LIMIT 20 ' +
     ') USR ' +
     'CROSS JOIN ( ' +
-        'SELECT P.ID, P.NAME, SUM(O.PRICE) AS PRODUCT_SUM ' +
+    'SELECT P.ID, P.NAME, SUM(O.PRICE) AS PRODUCT_SUM ' +
     'FROM PRODUCTS P, ORDERS O ' +
     'WHERE O.PRODUCT_ID = P.ID ' +
     'GROUP BY P.ID, P.NAME ' +
@@ -30,7 +31,7 @@ var top_k_user =
     'GROUP BY USR.ID, PROD.ID, USR.NAME, PROD.NAME, USR.USER_SUM, PROD.PRODUCT_SUM ' +
     'ORDER BY PROD.PRODUCT_SUM DESC, USR.USER_SUM DESC;';
 
-var top_k_product =
+var topKProduct =
     'SELECT PROD.NAME AS USER_NAME, USR.NAME AS PRODUCT_NAME, COALESCE(SUM(O.PRICE), 0) AS CELL_SUM, ' +
     'PROD.PRODUCT_SUM AS ROW_SUM, USR.USER_SUM AS COLUMN_SUM ' +
     'FROM ( ' +
@@ -52,42 +53,53 @@ var top_k_product =
     'GROUP BY USR.ID, PROD.ID, USR.NAME, PROD.NAME, USR.USER_SUM, PROD.PRODUCT_SUM ' +
     'ORDER BY PROD.PRODUCT_SUM DESC, USR.USER_SUM DESC;';
 
-var simple_query = 'select * from orders ';
+var simple_query = 'select u.name, p.name, o.quantity, o.price';
+var recentOrders = 'SELECT o.id, u.name AS user_name, p.name as product_name, o.quantity, o.price ' +
+    'FROM orders o LEFT JOIN users u ON o.user_id = u.id ' +
+    'LEFT JOIN products p ON o.product_id = p.id ' +
+    'ORDER BY o.id DESC ' +
+    'LIMIT 10;'
 
 var topkusersTable = new AnalyticsTable('topkusers');
 var topkproductsTable = new AnalyticsTable('topkproducts');
+var recentOrdersTable = new RecentOrdersTable('recentOrders');
 
-socket.emit('init query', {table: 'orders', query: top_k_user});
-socket.emit('init query', {table: 'orders', query: top_k_product});
-// socket.emit('init query', {table: 'orders', query: simple_query});
+socket.emit('init query', {table: 'orders', query: topKUser});
+socket.emit('init query', {table: 'orders', query: topKProduct});
+socket.emit('init query', {table: 'orders', query: recentOrders});
 
-socket.on('init query', request => {
+socket.on('init query', request =>
+{
     console.log('Populating table');
-    if(request.query === top_k_user){
+    if (request.query === topKUser) {
         topkusersTable.populate(request.payload, 'User \\ Product');
     }
-    if(request.query === top_k_product){
+    if (request.query === topKProduct) {
         topkproductsTable.populate(request.payload, 'Product \\ User');
     }
-    if(request.query === simple_query){
-        console.log(request);
+    if (request.query === recentOrders) {
+        // console.log(request);
+        recentOrdersTable.populate(request.payload);
     }
 });
 
-socket.on('error message', data => {
+socket.on('error message', data =>
+{
     console.error('OMG SOCKET ERROR: ', data)
 });
 
-var insertLog = new Log('insertLog');
-socket.on('diff query', request => {
-    if(request.query === top_k_user){
+var insertLog = new Log('diffLog');
+socket.on('diff query', request =>
+{
+    if (request.query === topKUser) {
         topkusersTable.updateTable(request.payload);
     }
-    if(request.query === top_k_product){
+    if (request.query === topKProduct) {
         topkproductsTable.updateTable(request.payload);
     }
-    if(request.query === simple_query){
-        console.log('DIFF INCOMING: ', request);
+    if (request.query === simple_query) {
+        // console.log('DIFF INCOMING: ', request);
+        recentOrdersTable.updateTable(request.payload);
     }
     insertLog.appendLog(request);
 });
